0d1524ded9dc60993d3e1f91083c56852040c389
talha4real <talhabutt993@gmail.com>
2022-01-22 02:06:24 +0500

    lightninghyper

---
+++ commands/key.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+module.exports = {
+    
+    name: 'key', // Command name (what's gonna be used to call the command)
+    aliases: ['keys'], // Command aliases
+  
+    execute(client, message) {
+
+      message.channel.send('Looking...').then(async (sentMsg) => {
+
+        try{
+            let key = message.content.split(' ');
+            await axios.get(`https://api.hyper.co/v4/licenses/${key[1]}`,{
+                headers:{
+                    Authorization: `Bearer pk_zddeWq1ccD48dVGi0V1T2skwolW9iaLN`
+                }
+            }).then(res=>{
+                sentMsg.edit(
+                    `Metadata: \`${
+                        JSON.stringify(res.data.metadata)
+                    }\``
+                  );
+                
+            })
+        }catch(e){
+            sentMsg.edit(
+                `Failed`
+              );
+        }
+     
+
+       
+      });
+    },
+  };
+
+  
